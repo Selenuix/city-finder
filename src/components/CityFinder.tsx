73b5c4068c46c1a1961818aff7e68fdb651bc400
Selenuix <anthony.ciszek@outlook.com>
2024-09-21 21:48:20 +0200

    fix(form): use shadcn form

--- src/components/CityFinder.tsx
+++ src/components/CityFinder.tsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
 import { actions } from "astro:actions";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 import type { City } from "../models/City";
 import { findBestMeetingCity } from "../utils/cities";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { Alert } from "@/components/ui/alert";
-import { CityForm } from "@/components/forms/CityForm.tsx";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form.tsx";
+import { Input } from "@/components/ui/input.tsx";
 
 interface CityFinderProps {
   apiKey: string;
@@ -17,6 +27,7 @@ export const CityFinder = ({ apiKey }: CityFinderProps) => {
   const [cityInput, setCityInput] = useState<string>("");
   const [bestCity, setBestCity] = useState<City | null>(null);
   const [error, setError] = useState<string>("");
+  const form = useForm();
 
   const handleAddCity = async () => {
     if (cityInput) {
@@ -56,7 +67,35 @@ export const CityFinder = ({ apiKey }: CityFinderProps) => {
     <div className="max-w-xl mx-auto p-4 bg-white rounded-lg shadow-md mt-10">
       <h1 className="text-2xl font-bold text-center mb-4">City Finder</h1>
       <div className="flex flex-col sm:flex-row items-center justify-between mb-4">
-        <CityForm handleAddCity={handleAddCity} />
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleAddCity)}
+            className="w-2/3 space-y-6"
+          >
+            <FormField
+              control={form.control}
+              name="city"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>City</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Paris"
+                      value={cityInput}
+                      onChange={(e) =>
+                        setCityInput((e.target as HTMLInputElement).value)
+                      }
+                      autoFocus={true}
+                    />
+                  </FormControl>
+                  <FormDescription>Add a city to the list.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
 
         {/* <Input
           className="flex-grow mr-2"
